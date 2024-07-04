cd110d89c6ed2493b059fefb49e23d73c099cfa4
zaycho <zaycho25@gmail.com>
2024-07-04 13:21:05 +0700

    feat: Add method to get customer by reference ID

--- lib/xendit_api/api/customer.rb
+++ lib/xendit_api/api/customer.rb
@@ -8,7 +8,15 @@ module XenditApi
 
       def create(params, headers = {})
         response = client.post(PATH, params, headers)
+
         XenditApi::Model::Customer.new(response)
+      end
+
+      def get_by_reference_id(reference_id, headers = {})
+        reference_id_path = "#{PATH}?reference_id=#{reference_id}"
+        response = client.get(reference_id_path, nil, headers)
+        # response is an array of customers
+        XenditApi::Model::Customer.new(response['data'][0])
       end
     end
   end
--- spec/xendit_api/api/customer_spec.rb
+++ spec/xendit_api/api/customer_spec.rb
@@ -2,24 +2,25 @@ require 'spec_helper'
 
 RSpec.describe XenditApi::Api::Customer do
   let(:client) { XenditApi::Client.new('xnd_development_oDjdREFnGLMrXV2PRIpcui5m9NNb00IfrweTBDMWauzk6SqaUaWxpxXB8qubECk') }
+
   describe '#create' do
     it 'returns expected response' do
       VCR.use_cassette('xendit/customer/create/success') do
         customer_api = described_class.new(client)
         headers = {
           for_user_id: '667d15f3e07834e30a69ff45',
-          IDEMPOTENCY_KEY: 'test-1234567890',
+          IDEMPOTENCY_KEY: 'idempotency-key-1234567895',
           API_Version: '2020-10-31'
         }
         params = {
-          reference_id: "test-1234567890",
-          type: "INDIVIDUAL",
+          reference_id: 'reference-id-1234567895',
+          type: 'INDIVIDUAL',
           individual_detail: {
-              given_names: "John",
-              surname: "Doe"
+            given_names: 'John',
+            surname: 'Doe'
           },
-          email: "testemail@email.com",
-          mobile_number: "+628774494404"
+          email: 'testemail@email.com',
+          mobile_number: '+628774494404'
         }
         response = customer_api.create(params, headers)
 
@@ -32,24 +33,24 @@ RSpec.describe XenditApi::Api::Customer do
         customer_api = described_class.new(client)
         headers = {
           for_user_id: '667d15f3e07834e30a69ff45',
-          IDEMPOTENCY_KEY: 'test-123456789',
+          IDEMPOTENCY_KEY: 'idempotency-key-1234567896',
           API_Version: '2020-10-31'
         }
         params = {
-          reference_id: "test-1234567890",
-          type: "INDIVIDUAL",
+          reference_id: 'reference-id-1234567895',
+          type: 'INDIVIDUAL',
           individual_detail: {
-              given_names: "John",
-              surname: "Doe"
+            given_names: 'John',
+            surname: 'Doe'
           },
-          email: "testemail@email.com",
-          mobile_number: "+628774494404"
+          email: 'testemail@email.com',
+          mobile_number: '+628774494404'
         }
 
         expect do
           customer_api.create(params, headers)
         end.to raise_error do |error|
-          expect(error).to be_kind_of XenditApi::Errors::DuplicateError
+          expect(error).to be_a XenditApi::Errors::DuplicateError
           expect(error.message).to eq 'The reference_id entered has been used before. Please enter a unique reference_id'
         end
       end
@@ -60,25 +61,42 @@ RSpec.describe XenditApi::Api::Customer do
         customer_api = described_class.new(client)
         headers = {
           for_user_id: '667d15f3e07834e30a69ff45',
-          IDEMPOTENCY_KEY: 'test-123456789',
+          IDEMPOTENCY_KEY: 'idempotency-key-1234567895',
           API_Version: '2020-10-31'
         }
         params = {
-          reference_id: "test-1234567890",
-          type: "INDIVIDUAL",
+          reference_id: 'reference-id-1234567896',
+          type: 'INDIVIDUAL',
           individual_detail: {
-              given_names: "John",
-              surname: "Doe"
+            given_names: 'John',
+            surname: 'Doe'
           },
-          email: "testemail@email.com",
-          mobile_number: "+628774494404"
+          email: 'testemail@email.com',
+          mobile_number: '+628774494404'
         }
 
         expect do
           customer_api.create(params, headers)
         end.to raise_error do |error|
-          expect(error).to be_kind_of XenditApi::Errors::IdempotencyError
+          expect(error).to be_a XenditApi::Errors::IdempotencyError
         end
+      end
+    end
+  end
+
+  describe '#get_by_reference_id' do
+    it 'returns expected response' do
+      VCR.use_cassette('xendit/customer/get_by_reference_id/success') do
+        customer_api = described_class.new(client)
+        headers = {
+          for_user_id: '667d15f3e07834e30a69ff45',
+          API_Version: '2020-10-31'
+        }
+        reference_id = 'reference-id-1234567895'
+        response = customer_api.get_by_reference_id(reference_id, headers)
+
+        expect(response).to be_instance_of XenditApi::Model::Customer
+        expect(response.reference_id).to eq 'reference-id-1234567895'
       end
     end
   end
